d10fbda27554f9b3387f1e80bf28c746b5f9095b
Gibshosting <gibrandwisatrio1305@gmail.com>
2025-11-08 13:26:13 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -178,7 +178,7 @@ app.post("/encrypt", upload.single("file"), async (req, res) => {
       try {
         // run with timeout to avoid long blocking
         resultCode = await withTimeout(
-          obfuscator.obfuscateCode(code, preset, { includeAntiBypass, password }),
+          obfuscator.obfuscateCode(code, preset, { includeAntiBypass,includeBypass, password }),
           OBF_TIMEOUT_MS
         );
       } catch (err) {
